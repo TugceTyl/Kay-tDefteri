04a91da30ff62a32d9f37ce4380ea5c66f021324
DELL <DELL@DESKTOP-FACA332>
2023-09-01 10:27:05 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace KayıtDefteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox3 = new TextBox();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(22, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Adı Soyadı:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(139, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(207, 308);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 50);
+            button1.TabIndex = 2;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(445, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Adı Soyadı:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(593, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 28);
+            label3.TabIndex = 4;
+            label3.Text = "00";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(139, 93);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(168, 34);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(22, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Telefon:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(593, 93);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 28);
+            label5.TabIndex = 8;
+            label5.Text = "00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(445, 93);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 28);
+            label6.TabIndex = 7;
+            label6.Text = "Telefon:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Bostancı", "Kartal", "Kadıköy", "Adalar", "Şişli", "Beyoğlu" });
+            comboBox1.Location = new Point(139, 143);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(168, 36);
+            comboBox1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(39, 146);
+            label7.Name = "label7";
+            label7.Size = new Size(61, 28);
+            label7.TabIndex = 10;
+            label7.Text = "Semt:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(593, 131);
+            label8.Name = "label8";
+            label8.Size = new Size(36, 28);
+            label8.TabIndex = 12;
+            label8.Text = "00";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(445, 131);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 28);
+            label9.TabIndex = 11;
+            label9.Text = "Semt:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(142, 202);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(168, 87);
+            textBox3.TabIndex = 14;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(22, 202);
+            label10.Name = "label10";
+            label10.Size = new Size(66, 28);
+            label10.TabIndex = 13;
+            label10.Text = "Adres:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(593, 176);
+            label11.Name = "label11";
+            label11.Size = new Size(36, 28);
+            label11.TabIndex = 16;
+            label11.Text = "00";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(445, 176);
+            label12.Name = "label12";
+            label12.Size = new Size(71, 28);
+            label12.TabIndex = 15;
+            label12.Text = "Adres:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 128, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(textBox3);
+            Controls.Add(label10);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Kayıt Defteri";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox1;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox3;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+    }
+}
